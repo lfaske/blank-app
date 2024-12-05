5d6f29db5fcc0a5591f833107619f18ced22a695
lfaske <103024385+lfaske@users.noreply.github.com>
2024-12-05 10:56:15 +0000

    First Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new chat app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start testing! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+st.write(
+    "This is a simple test"
 )
